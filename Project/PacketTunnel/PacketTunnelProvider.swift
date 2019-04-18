fae304db1b047cf4b19df02c2f3d95323f13d07f
itsjohnye <yzz1015@hotmail.com>
2019-04-18 09:01:57 +0800

    update

--- Project/PacketTunnel/PacketTunnelProvider.swift
+++ Project/PacketTunnel/PacketTunnelProvider.swift
@@ -25,7 +25,7 @@ class PacketTunnelProvider: NEPacketTunnelProvider {
 
 	override func startTunnel(options: [String : NSObject]?, completionHandler: @escaping (Error?) -> Void) {
         DDLog.removeAllLoggers()
-        DDLog.add(DDASLLogger.sharedInstance, with: DDLogLevel.info)
+        DDLog.add(DDOSLogger.sharedInstance, with: DDLogLevel.info)
         ObserverFactory.currentFactory = DebugObserverFactory()
         NSLog("-------------")
         
@@ -95,8 +95,8 @@ class PacketTunnelProvider: NEPacketTunnelProvider {
                     let raw_dom = dom.string!
                     let index = raw_dom.index(raw_dom.startIndex, offsetBy: 1)
                     let index2 = raw_dom.index(raw_dom.startIndex, offsetBy: 2)
-                    let typeStr = raw_dom.substring(to: index)
-                    let url = raw_dom.substring(from: index2)
+                    let typeStr = String(raw_dom[..<index])
+                    let url = String(raw_dom[index2...])
                     
                     if typeStr == "s"{
                         rule_array.append(DomainListRule.MatchCriterion.suffix(url))
